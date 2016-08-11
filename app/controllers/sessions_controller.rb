9715584ab49401bccce570f7dd13980a4aa08d78
Andrew Vathanakamsang <email": null>
2016-08-10 17:46:32 -0700

    Add way to authenticate user and error handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
   def create
-    render text: request.env['omniauth.auth'].to_yaml and return
-    # begin
-    #   @user = User.from_omniauth(request.env['omniauth.auth'])
-    #   session[:user_id] = @user.id
-    #   flash[:success] = "Welcome, #{@user.name}!"
-    # rescue
-    #  flash[:warning] = "There was an error while trying to authenticate you..."
-    # end
-    # redirect_to root_path
+    # render text: request.env['omniauth.auth'].to_yaml and return
+    begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.name}!"
+    rescue
+     flash[:warning] = "There was an error while trying to authenticate you..."
+    end
+    redirect_to root_path
   end
 
   def destroy
