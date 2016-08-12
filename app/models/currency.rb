8b7021ebfd29daafb36b0ee921fc48b9955998a0
Andrew Vathanakamsang <email": null>
2016-08-12 16:32:52 -0700

    Create relationship between fxtran and currencies

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,4 @@
 class Currency < ActiveRecord::Base
+	has_many :base_fxtran, :class_name => "Fxtran", :foreign_key => "base_curr_id"
+	has_many :quote_fxtran, :class_name => "Fxtran", :foreign_key => "quote_curr_id"
 end
--- app/models/fxtran.rb
+++ app/models/fxtran.rb
@@ -1,4 +1,5 @@
 class Fxtran < ActiveRecord::Base
 	belongs_to :status
-
+	belongs_to :base_curr, :class_name => "Currency"
+	belongs_to :quote_curr, :class_name => "Currency"
 end
