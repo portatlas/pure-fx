83c10e2904c6fde337b9958c22ba2f0553e37a64
Andrew Vathanakamsang <email": null>
2016-08-12 14:23:04 -0700

    Create a currency model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811212759) do
+ActiveRecord::Schema.define(version: 20160812212140) do
+
+  create_table "currencies", force: :cascade do |t|
+    t.string   "curr_code",  null: false
+    t.string   "curr_name",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "fxtrans", force: :cascade do |t|
     t.integer  "status_id",                              null: false
