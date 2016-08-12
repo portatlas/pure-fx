88fde9bff00706bd000e27d96be93e93ff549430
Andrew Vathanakamsang <email": null>
2016-08-12 13:11:16 -0700

    Create the create method for FxtransController

--- app/controllers/fxtrans_controller.rb
+++ app/controllers/fxtrans_controller.rb
@@ -1,2 +1,18 @@
 class FxtransController < ApplicationController
+
+	def create
+		@fxtran = Fxtran.new(fxtran_params)
+		if @fxtran.save
+			redirect_to root_path
+		else
+			render "new"
+		end
+	end
+
+	private
+
+	def fxtran_params
+		params.require(:fxtran).permit(:status_id, :requestor_id, :base_amount, :base_curr_id, :quote_curr_id, :created_at, :updated_at)
+	end
+
 end
