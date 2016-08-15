f92554700d9fdea0198f988c7bf0dd9cad3303be
Andrew Vathanakamsang <email": null>
2016-08-15 12:14:15 -0700

    Clean up code and assign lat, lng, and accuracy to an fxtransaction

--- app/controllers/fxtrans_controller.rb
+++ app/controllers/fxtrans_controller.rb
@@ -1,15 +1,14 @@
 class FxtransController < ApplicationController
-	# attr_accessible :city, :lat, :lng
-	# geocoded_by :
-	# after_validation :geocode
 
 	def new
 		@fxtran = Fxtran.new
-		# @currencies = Currency.all
 	end
 
 	def create
 		@fxtran = Fxtran.new(fxtran_params)
+		@fxtran.lat = cookies[:latitude]
+		@fxtran.lng = cookies[:longitude]
+		@fxtran.accry = cookies[:accuracy]
 		if @fxtran.save
 			redirect_to root_path
 		else
@@ -20,7 +19,7 @@ class FxtransController < ApplicationController
 	private
 
 	def fxtran_params
-		params.require(:fxtran).permit(:status_id, :requestor_id, :base_amount, :base_curr_id, :quote_curr_id, :lat, :lng, :accry, :created_at, :updated_at)
+		params.require(:fxtran).permit(:status_id, :requestor_id, :base_amount, :base_curr_id, :quote_curr_id, :city, :lat, :lng, :accry, :created_at, :updated_at)
 	end
 
 end
