0f45d7804d1c5495d9c5c444802a1bf49dd380fd
Andrew Vathanakamsang <email": null>
2016-08-15 12:45:14 -0700

    Create associations for fxtrans with users

--- app/models/fxtran.rb
+++ app/models/fxtran.rb
@@ -1,4 +1,6 @@
 class Fxtran < ActiveRecord::Base
+	belongs_to :requestor, {class_name: "User", foreign_key: :requestor_id}
+  belongs_to :acceptor, {class_name: "User", foreign_key: :acceptor_id}
 	belongs_to :status
 	belongs_to :base_curr, :class_name => "Currency"
 	belongs_to :quote_curr, :class_name => "Currency"
