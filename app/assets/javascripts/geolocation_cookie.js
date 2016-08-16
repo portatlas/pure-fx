6c5cc7d0f47ae4ae292fb9e5507047732951877b
Andrew Vathanakamsang <email": null>
2016-08-15 19:31:40 -0700

    Troubleshoot js for geolocation

--- app/assets/javascripts/geolocation_cookie.js
+++ app/assets/javascripts/geolocation_cookie.js
@@ -1,27 +1,33 @@
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maxmiumAge: 0
-};
-
-function success(pos){
-  var crd = pos.coords
-
-  console.log('Coordinates are:');
-  console.log('Latitude: ' + crd.latitude);
-  console.log('Longitude: ' + crd.longitude);
-  console.log('Accuracy: ' + crd.accuracy);
-
-  document.cookie = "latitude=" + crd.latitude;
-  document.cookie = "longitude=" + crd.longitude;
-  document.cookie = "accuracy=" + crd.accuracy;
-};
-
-function error(err){
-  console.warn('ERROR(' + err.code + '): ' + err.message);
+navigator.geolocation.getCurrentPosition(GeoL);
+function GeoL(position) {
+     document.getElementById('lat').value = position.coords.latitude;
+     document.getElementById('lng').value = position.coords.longitude;
 }
 
-$(".fx-submit").click(function(event){
-  console.log("it is loaded yes")
-  navigator.geolocation.getCurrentPosition(success, error, options)
-});
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maxmiumAge: 0
+// };
+
+// function success(pos){
+//   var crd = pos.coords
+
+//   console.log('Coordinates are:');
+//   console.log('Latitude: ' + crd.latitude);
+//   console.log('Longitude: ' + crd.longitude);
+//   console.log('Accuracy: ' + crd.accuracy);
+
+//   document.cookie = "latitude=" + crd.latitude;
+//   document.cookie = "longitude=" + crd.longitude;
+//   document.cookie = "accuracy=" + crd.accuracy;
+// };
+
+// function error(err){
+//   console.warn('ERROR(' + err.code + '): ' + err.message);
+// }
+
+// $(".fx-submit").click(function(){
+//   console.log("it is loaded yes")
+//   navigator.geolocation.getCurrentPosition(success, error, options)
+// });
