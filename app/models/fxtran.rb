75d7a2a9282719ce18bf843aed84b8580b6b1b90
Andrew Vathanakamsang <email": null>
2016-08-15 19:07:46 -0700

    Add backend validation to ensure user input passes data standards

--- app/models/fxtran.rb
+++ app/models/fxtran.rb
@@ -4,4 +4,10 @@ class Fxtran < ActiveRecord::Base
 	belongs_to :status
 	belongs_to :base_curr, :class_name => "Currency"
 	belongs_to :quote_curr, :class_name => "Currency"
+
+	validates :base_amount, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+	validates :base_curr_id, presence: true
+	validates :quote_curr_id, presence: true
+	validates :lat, presence: true
+	validates :lng, presence: true
 end
